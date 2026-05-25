5a398592c4f6309076377ce951335297b4adb918
Niranjan Shrestha <niranjansthaa11@gmail.com>
2026-05-25 10:17:30 +0545

    added some small changes and made the error thing shows up when the users eneter the wrong input i.e city not available

--- app.js
+++ app.js
@@ -6,14 +6,45 @@ const searchBtn = document.querySelector(".search button")
 async function checkwheather(city) {
     const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
     var data = await response.json();
-
+    
+    //making the not data found type of thing when user eneters the wrong data 
+    if(data.cod == "404"){
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".wheather").style.display ="none";
+    }else{
+        document.querySelector(".wheather").style.display ="block";
+    }
 
     console.log(data);
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round( data.main.temp )+ "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
     document.querySelector(".hawa").innerHTML = data.wind.speed +"km/hr" ;
+    const wheather_status = document.querySelector(".wheather_kasto_xw_icon")
+// For the image change thing 
+if(data.weather[0].main == "Clouds"){
+    wheather_status.src="images/clouds.png"
 }
+else if(data.weather[0].main == "Rain"){
+    wheather_status.src="images/rain.png"
+}
+else if(data.weather[0].main == "Clear"){
+    wheather_status.src="images/clear.png"
+}
+else if(data.weather[0].main == "Drizzle"){
+    wheather_status.src="images/drizzle.png"
+}
+else if(data.weather[0].main == "Mist"){
+    wheather_status.src="images/mist.png"
+}
+
+document.querySelector(".wheather").style.display ="block";
+document.querySelector(".error").style.display="none";
+}
+
+
+
+
 
 searchBtn.addEventListener("click", ()=> {
     checkwheather(searchBox.value);
